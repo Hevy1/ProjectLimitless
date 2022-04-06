a392b1de09aa9d280dacfe03420eceb7ee1598a2
Hevy1 <evan.weisse@outlook.fr>
2022-04-06 09:17:30 -0400

    Ajout du mouvement de la cam pour le joueur test

--- ProjectLimitless/Assets/Scripts/Player.cs
+++ ProjectLimitless/Assets/Scripts/Player.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private const float Y_MAX_ANGLE = 50f;
+    private const float Y_MIN_ANGLE = 0f;
 
     public CharacterController controller;
+    public Transform camTransform;
+    public Transform lookAt;
     public float speed = 12f;
     public float gravity = -9.81f;
     public float jumpHeight = 3f;
+    public float distance = 3f;
     Vector3 velocity;
+    Vector2 currentRot;
 
     // Start is called before the first frame update
     void Start()
@@ -31,5 +37,12 @@ public class Player : MonoBehaviour
 
         controller.Move(movement);
         controller.Move(velocity * Time.deltaTime);
+
+        Vector3 dir = new Vector3(0,0, - distance);
+        currentRot += new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        currentRot.y = Mathf.Clamp(currentRot.y, Y_MIN_ANGLE, Y_MAX_ANGLE);
+        Quaternion rotation = Quaternion.Euler(currentRot.y, currentRot.x, 0);
+        camTransform.position = lookAt.position + rotation * dir;
+        camTransform.LookAt(lookAt.position);
     }
 }
