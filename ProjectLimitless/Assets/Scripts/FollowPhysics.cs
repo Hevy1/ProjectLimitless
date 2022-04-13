b60ca799f894ccc9a79f7ce843b84da3317dc230
Léo Vansimay <l.vansimay@gmail.com>
2022-04-12 20:05:05 -0400

    Portes
    ajout d'assets et création d'un prefab avec les contraintes physiques pour l'ouverture et fermeture de la porte

---
+++ ProjectLimitless/Assets/Scripts/FollowPhysics.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPhysics : MonoBehaviour
+{
+
+    public Transform target;
+
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.MovePosition(target.transform.position);
+    }
+}
