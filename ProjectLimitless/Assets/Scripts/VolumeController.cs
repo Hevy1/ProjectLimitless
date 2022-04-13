ff9ca6f3a92281bf291495cdada26b18972f09e9
ThomasClvr <99747179+ThomasClvr@users.noreply.github.com>
2022-04-13 04:53:37 +0200

    Correction bug

--- ProjectLimitless/Assets/Scripts/VolumeController.cs
+++ ProjectLimitless/Assets/Scripts/VolumeController.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class VolumeController : MonoBehaviour
+public class VolumeSaveController : MonoBehaviour
 {
+    [SerializeField] private Slider volumeSlider = null;
 
     [SerializeField] private Text volumeTextUI = null;
 
     public void VolumeSlider(float volume)
     {
         volumeTextUI.text = volume.ToString("0.0");
+
     }
 }
